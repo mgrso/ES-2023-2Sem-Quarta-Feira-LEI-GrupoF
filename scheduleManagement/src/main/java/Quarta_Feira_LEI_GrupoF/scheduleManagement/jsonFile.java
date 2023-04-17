9d2bd7b49be753f849c5bca8e579fb0e8ec5e2a1
Crossfade1 <rui.c.2k11@gmail.com>
2023-04-17 02:49:52 +0100

    jsonFile update to use string lists

--- scheduleManagement/src/main/java/Quarta_Feira_LEI_GrupoF/scheduleManagement/jsonFile.java
+++ scheduleManagement/src/main/java/Quarta_Feira_LEI_GrupoF/scheduleManagement/jsonFile.java
@@ -9,27 +9,27 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class jsonFile {
-	
-	private List<Object> data;
-	
-	public jsonFile(String filename) throws IOException {
-		// Read the JSON file and deserialize its contents into a list of objects
-		ObjectMapper mapper = new ObjectMapper();
-		this.data = mapper.readValue(new File(filename), new TypeReference<List<Object>>() {});
-	}
-	
-	public jsonFile(List<Object> data) {
-		// Initialize the JsonFile with a list of objects
-		this.data = data;
-	}
-	
-	public List<Object> getList() {
-		return this.data;
-	}
-	
-	public void saveFile(String newFileName) throws JsonProcessingException, IOException {
-		// Serialize the data list to JSON and write it to the specified file
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.writerWithDefaultPrettyPrinter().writeValue(new File(newFileName), this.data);
-	}
+    
+    private List<String> data;
+    
+    public jsonFile(String filename) throws IOException {
+        // Read the JSON file and deserialize its contents into a list of strings
+        ObjectMapper mapper = new ObjectMapper();
+        this.data = mapper.readValue(new File(filename), new TypeReference<List<String>>() {});
+    }
+    
+    public jsonFile(List<String> data) {
+        // Initialize the JsonFile with a list of strings
+        this.data = data;
+    }
+    
+    public List<String> getList() {
+        return this.data;
+    }
+    
+    public void saveFile(String newFileName) throws JsonProcessingException, IOException {
+        // Serialize the data list to JSON and write it to the specified file
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writerWithDefaultPrettyPrinter().writeValue(new File(newFileName), this.data);
+    }
 }
